dc83610e325d0b9b31ba21bc520b548ff0caf2e2
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-07-25 23:21:56 +0400

    initialize state & updated state properties

--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ import ReactDOM from 'react-dom/client';
 import SeasonsDisplay from "./SeasonsDisplay"
 import './App.css'
 
-const App = () => {
-    //call the Geolocation API
-    window.navigator.geolocation.getCurrentPosition(
-        //when the API call is successful
-        position => console.log(position),
 
-        //when it doesn't quite work out
-        err => console.log(err)
-    )
+//create a class component
+class App extends React.Component {
+    //initialize state
+    constructor(props) {
+        super(props)
 
-    return (
-        // <div>Our new weather app</div>
-        <SeasonsDisplay />
+        this.state = { latitude: null}
 
-    )
+        //call the Geolocation API
+        window.navigator.geolocation.getCurrentPosition(
+            //when the API call is successful
+            position => {
+                this.setState({ latitude: position.coords.latitude })
+            },
+            //when it doesn't quite work out
+            err => {}
+        )
+    }
+
+    render() {
+         //render our component and reference state
+        return <div>Latitude: {this.state.latitude}</div>
+    }
 }
 
 
