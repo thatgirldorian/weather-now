4fed02e627bc11d7d3b34c5bc7439ed1d2075b9a
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-07-27 00:07:06 +0400

    Added some custom styling to make page nicer-looking

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import './SeasonDisplay.css'
 
 //create a configuration object for conditional rendering
 const seasonConfig = {
     summer: {
         seasonText: "Lets' go to the beach, eachh",
-        seasonIcon: <i className="sun icon"></i>
+        seasonIcon: <i className="massive sun icon"></i>
     },
     winter: {
         seasonText: "It's so cold outside",
-        seasonIcon: <i className="snowflake icon"></i>
+        seasonIcon: <i className="massive snowflake icon"></i>
     }
 }
 
 //determine which season it is 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
-        return lat > 0 ? 'summer' :'winter'
+        return lat > 0 ? 'summer' :'winter';
     } else {
-        return lat < 0 ? 'winter' : 'summer'
+        return lat < 0 ? 'winter' : 'summer';
     }
 
     
@@ -28,10 +29,10 @@ const SeasonsDisplay = (props) => {
     const { seasonText, seasonIcon } = seasonConfig[season]
 
     return (
-        <div>
-            <h1>{seasonIcon}</h1>
+        <div className={`season-display ${season}`}>
+            <i className="icon-left">{seasonIcon}</i>
             <h1>{seasonText}</h1>
-            <h1>{seasonIcon}</h1>
+            <i className="icon-right">{seasonIcon}</i>
         </div>
     )
 }
